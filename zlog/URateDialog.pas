14f77d37dfa72ec3db2aa54081fd8801c5ff26fa
jr8ppg <jr8ppg@gmail.com>
2020-05-02 23:39:22 +0900

    Changed: QSO Rateウインドウの近代化① ・TChartコンポーネントを利用 ・グラフの棒にQSO数を表示 ・累積交信数グラフ（折れ線） ・リサイズ可能ウインドウ

--- zlog/URateDialog.pas
+++ zlog/URateDialog.pas
@@ -2,8 +2,11 @@ unit URateDialog;
 
 interface
 
-uses Windows, SysUtils, Classes, Graphics, Forms, Controls, StdCtrls,
-  Buttons, ExtCtrls, wsaGraph, UOptions, UzLogGlobal;
+uses
+  Windows, SysUtils, Classes, Graphics, Forms, Controls, StdCtrls,
+  Buttons, ExtCtrls, System.Math,
+  VclTee.TeeGDIPlus, VCLTee.TeEngine, VCLTee.TeeProcs, VCLTee.Chart,
+  UOptions, UzLogGlobal, VCLTee.Series;
 
 type
   TRateDialog = class(TForm)
@@ -18,30 +21,31 @@ type
     Panel2: TPanel;
     OKBtn: TButton;
     StayOnTop: TCheckBox;
-    Graph: TwsaGraph;
-    Panel3: TPanel;
+    Chart1: TChart;
+    Series1: TBarSeries;
+    Series2: TLineSeries;
+    Label4: TLabel;
     ShowLastCombo: TComboBox;
     Label3: TLabel;
-    Label4: TLabel;
+    check3D: TCheckBox;
+    procedure CreateParams(var Params: TCreateParams); override;
+    procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure TimerTimer(Sender: TObject);
     procedure OKBtnClick(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
-    procedure FormKeyDown(Sender: TObject; var Key: Word;
-      Shift: TShiftState);
     procedure StayOnTopClick(Sender: TObject);
-    procedure FormShow(Sender: TObject);
     procedure ShowLastComboChange(Sender: TObject);
-    procedure FormClose(Sender: TObject; var Action: TCloseAction);
-    procedure CreateParams(var Params: TCreateParams);
+    procedure check3DClick(Sender: TObject);
   private
-    _max10, _max100 : double;
-    function CountQSOs(_start, _end : TDateTime) : integer;
     { Private declarations }
+    FLast10QsoRateMax: Double;
+    FLast100QsoRateMax: Double;
+    FShowLast: Integer;      { Show last x hours. default = 12}
   public
-     ShowLast : integer;      { Show last x hours. default = 12}
-     TimeIncrement : integer; { time increment in minutes default = 60}
-     procedure UpdateGraph;
     { Public declarations }
+    procedure UpdateGraph;
   end;
 
 var
@@ -49,22 +53,102 @@ var
 
 implementation
 
-uses Main;
+uses
+  Main;
 
 {$R *.DFM}
 
-procedure TRateDialog.TimerTimer(Sender: TObject);
-var Last : TDateTime;
-    Diff, Rate : double;
-    temp : string;
-    i : LongInt;
-    mytx, k : integer;
-    aQSO : TQSO;
+procedure TRateDialog.CreateParams(var Params: TCreateParams);
 begin
-   if not(Visible) then exit;
+   inherited CreateParams(Params);
+   Params.ExStyle := Params.ExStyle or WS_EX_APPWINDOW;
+end;
+
+procedure TRateDialog.FormCreate(Sender: TObject);
+begin
+   FLast10QsoRateMax := 0;
+   FLast100QsoRateMax := 0;
+   FShowLast := 12;
+   ShowLastCombo.ItemIndex := 2;
+
+   with Chart1 do begin
+      // グラフ全体
+      Title.Caption := '';
+      Title.Font.Size := 8;
+      Legend.Visible := False;
+
+      // 縦軸（時間毎の交信局数）の目盛り設定
+      Axes.Left.Automatic := False;
+      Axes.Left.Title.Caption := '';  //'時間毎の交信局数';
+      Axes.Left.Title.Font.Size := 8;
+      Axes.Left.Maximum := 10;
+      Axes.Left.Minimum := 0;
+      Axes.Left.Increment := 2;
+      Axes.Left.Grid.Visible := False;
+      Axes.Left.MinorTickCount := 0;
+
+      // 縦軸（累計）の目盛り設定
+      Axes.Right.Automatic := False;
+      Axes.Right.Title.Caption := ''; //'交信局数の累計';
+      Axes.Right.Title.Font.Size := 8;
+      Axes.Right.Maximum := 10;
+      Axes.Right.Minimum := 0;
+      Axes.Right.Increment := 5;
+      Axes.Right.Grid.Visible := False;
+      Axes.Right.MinorTickCount := 0;
+
+      // 横軸目盛りの設定
+      Axes.Bottom.Title.Caption := '';
+      Axes.Bottom.Title.Font.Size := 8;
+      Axes.Bottom.MinorTickCount := 0;
+   end;
+
+   with Series1 do begin
+      Clear();
+      VertAxis := aLeftAxis;
+      ValueFormat := '#,###';    // 0を出さない
+   end;
+
+   with Series2 do begin
+      Clear();
+      VertAxis := aRightAxis;
+   end;
+end;
+
+procedure TRateDialog.FormShow(Sender: TObject);
+begin
+   UpdateGraph;
+   Timer.Enabled := True;
+end;
+
+procedure TRateDialog.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+begin
+   case Key of
+      VK_ESCAPE:
+         MainForm.LastFocus.SetFocus;
+   end;
+end;
+
+procedure TRateDialog.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+   Timer.Enabled := False;
+end;
+
+procedure TRateDialog.TimerTimer(Sender: TObject);
+var
+   Last: TDateTime;
+   Diff, Rate: double;
+   i: LongInt;
+   mytx, k: integer;
+   aQSO: TQSO;
+begin
+   if Visible = False then begin
+      Exit;
+   end;
+
    i := Log.TotalQSO;
    if i < 10 then begin
-      exit;
+      Exit;
    end;
 
    mytx := dmZlogGlobal.TXNr;
@@ -79,184 +163,171 @@ begin
       dec(i)
    until (i = 0) or (k = 10);
 
-  if (k = 10) then
-    begin
+   if (k = 10) then begin
       Last := aQSO.QSO.time;
-      Diff := (CurrentTime - Last) * 24.0 ;
+      Diff := (CurrentTime - Last) * 24.0;
       Rate := 10 / Diff;
-      if Rate > _Max10 then
-        _Max10 := Rate;
-      Str(Rate : 3:2, temp);
-      Last10.Caption := temp + ' QSOs/hr';
-      Str(_Max10 : 3:2, temp);
-      Max10.Caption := 'max '+temp + ' QSOs/hr';
-    end
-  else
-    exit;
 
-  i := Log.TotalQSO;
-  k := 0;
-  repeat
-    aQSO := TQSO(Log.List[i]);
-    if aQSO.QSO.TX = mytx then
-      begin
-        inc(k);
+      FLast10QsoRateMax := Max(FLast10QsoRateMax, Rate);
+
+      Last10.Caption := Format('%3.2f', [Rate]) + ' QSOs/hr';
+      Max10.Caption := 'max ' + Format('%3.2f', [FLast10QsoRateMax]) + ' QSOs/hr';
+   end
+   else begin
+      Exit;
+   end;
+
+   i := Log.TotalQSO;
+   k := 0;
+   repeat
+      aQSO := TQSO(Log.List[i]);
+      if aQSO.QSO.TX = mytx then begin
+         inc(k);
       end;
-    dec(i)
-  until (i = 0) or (k = 100);
+      dec(i)
+   until (i = 0) or (k = 100);
 
-  if k = 100 then
-    begin
+   if k = 100 then begin
       Last := aQSO.QSO.time;
-      Diff := (CurrentTime - Last) * 24.0 ;
+      Diff := (CurrentTime - Last) * 24.0;
       Rate := 100 / Diff;
-      If Rate > _Max100 then
-        _Max100 := Rate;
-      Str(Rate : 3:2, temp);
-      Last100.Caption := temp + ' QSOs/hr';
-      Str(_Max100 : 3:2, temp);
-      Max100.Caption := 'max '+temp + ' QSOs/hr';
-    end;
 
+      FLast100QsoRateMax := Max(FLast100QsoRateMax, Rate);
 
-{
-  if i >= 10 then
-    begin
-      Last := TQSO(Log.List[i-9]).QSO.time;
-      Diff := (CurrentTime - Last) * 24.0 ;
-      Rate := 10 / Diff;
-      if Rate > _Max10 then
-        _Max10 := Rate;
-      Str(Rate : 3:2, temp);
-      Last10.Caption := temp + ' QSOs/hr';
-      Str(_Max10 : 3:2, temp);
-      Max10.Caption := 'max '+temp + ' QSOs/hr';
-    end;
-  if i >= 100 then
-    begin
-      Last := TQSO(Log.List[i-99]).QSO.time;
-      Diff := (CurrentTime - Last) * 24.0 ;
-      Rate := 100 / Diff;
-      If Rate > _Max100 then
-        _Max100 := Rate;
-      Str(Rate : 3:2, temp);
-      Last100.Caption := temp + ' QSOs/hr';
-      Str(_Max100 : 3:2, temp);
-      Max100.Caption := 'max '+temp + ' QSOs/hr';
-    end;
-}
-
+      Last100.Caption := Format('%3.2f', [Rate]) + ' QSOs/hr';
+      Max100.Caption := 'max ' + Format('%3.2f', [FLast100QsoRateMax]) + ' QSOs/hr';
+   end;
 end;
 
 procedure TRateDialog.OKBtnClick(Sender: TObject);
 begin
-  Close;
-  MainForm.LastFocus.SetFocus;
-end;
-
-procedure TRateDialog.FormCreate(Sender: TObject);
-begin
-  _max10 := 0;
-  _max100 := 0;
-  ShowLast := 12;
-  TimeIncrement := 60;
-  Graph.BackGroundColor := clBtnFace;
-  Graph.GraphColor := clNavy;
-end;
-
-procedure TRateDialog.FormKeyDown(Sender: TObject; var Key: Word;
-  Shift: TShiftState);
-begin
-  case Key of
-    VK_ESCAPE : MainForm.LastFocus.SetFocus;
-  end;
+   Close;
+   MainForm.LastFocus.SetFocus;
 end;
 
 procedure TRateDialog.StayOnTopClick(Sender: TObject);
 begin
-  If StayOnTop.Checked then
-    FormStyle := fsStayOnTop
-  else
-    FormStyle := fsNormal;
+   If StayOnTop.Checked then
+      FormStyle := fsStayOnTop
+   else
+      FormStyle := fsNormal;
 end;
 
-function TRateDialog.CountQSOs(_start, _end : TDateTime) : integer;
-var i : integer;
-    T : TDateTime;
+procedure TRateDialog.check3DClick(Sender: TObject);
 begin
-  Result := 0;
-  for i := 1 to Log.TotalQSO do
-    begin
-      T := TQSO(Log.List[i]).QSO.Time;
-      if (T >= _start) and (T < _end) then
-        inc(Result);
-    end;
+   Chart1.View3D := TCheckBox(Sender).Checked;
 end;
 
 procedure TRateDialog.UpdateGraph;
-var j, k : Integer;
-    str : string;
-    _Now : TDateTime;
-    _Start : TDateTime;
-
-var H, M, S, ms : word;
-
+var
+   hour_count: Integer;
+   total_count: Integer;
+   hour_peak: Integer;
+   Str: string;
+   _start: TDateTime;
+   H, M, S, ms: Word;
+   i: Integer;
+   aQSO: TQSO;
+   diff: TDateTime;
+   count_array: array[0..48] of Integer;
 begin
-  Graph.ClearGraph;
-  _Now := CurrentTime;
-  _Start := _Now - (ShowLast - 1) / 24;
-  DecodeTime(_Start,H, M, S, ms);
-  _Start := Int(_Start) + EncodeTime(H, 0, 0, 0);
+   Series1.Clear();
+   Series2.Clear();
+   Chart1.Axes.Bottom.Items.Clear();
 
-  if Log.TotalQSO = 0 then
-    exit;
-  if TQSO(Log.List[Log.TotalQSO]).QSO.Time < _Start then
-    exit;
+   _start := CurrentTime() - (FShowLast - 1) / 24;
+   DecodeTime(_start, H, M, S, ms);
+   _start := Int(_start) + EncodeTime(H, 0, 0, 0);
 
-  for k := 0 to ShowLast - 1 do
-    begin
-      j := CountQSOs(_Start + (1 / 24)*k, _Start + (1 / 24)*(k + 1));
-      str := IntToStr(GetHour(_Start + (1 / 24)*k));
-      if ShowLast > 12 then
-        if (GetHour(_Start + (1 / 24) * k) mod 2) = 1 then
-          str := '';
-      if ShowLast > 24 then
-        if (GetHour(_Start + (1 / 24) * k) mod 4) <> 0 then
-          str := '';
-      Graph.AddData(k + 1, j, str);
-    end;
-  Graph.PlotGraph;
-end;
+//   if Log.TotalQSO = 0 then begin
+//      Exit;
+//   end;
 
+//   if TQSO(Log.List[Log.TotalQSO]).QSO.time < _start then begin
+//      Exit;
+//   end;
 
-procedure TRateDialog.FormShow(Sender: TObject);
-begin
-  UpdateGraph;
-  Timer.Enabled := True;
+   for i := 0 to 48 do begin
+      count_array[i] := 0;
+   end;
+
+   total_count := 0;
+   for i := 1 to Log.TotalQSO do begin
+      aQSO := TQSO(Log.List[i]);
+
+      if (aQSO.QSO.Points = 0) then begin    // 得点無しはスキップ
+         Continue;
+      end;
+
+      if (aQSO.QSO.Time < _start) then begin // グラフ化以前の交信
+         Inc(total_count);
+      end
+      else begin
+         diff := aQSO.QSO.Time - _start;
+         DecodeTime(diff, H, M, S, ms);
+         if (H > 47) then begin
+            Continue;
+         end;
+
+         Inc(count_array[H]);
+      end;
+   end;
+
+   hour_peak := 0;
+   for i := 0 to FShowLast - 1 do begin
+      hour_count := count_array[i];
+
+      Str := IntToStr(GetHour(_start + (1 / 24) * i));
+
+      if FShowLast > 12 then begin
+         if (GetHour(_start + (1 / 24) * i) mod 2) = 1 then begin
+            Str := '';
+         end;
+      end;
+
+      if FShowLast > 24 then begin
+         if (GetHour(_start + (1 / 24) * i) mod 4) <> 0 then begin
+            Str := '';
+         end;
+      end;
+
+      // 縦軸目盛り調整のための値
+      total_count := total_count + hour_count;
+      hour_peak := Max(hour_peak, hour_count);
+
+      // 横軸目盛ラベル
+      Chart1.Axes.Bottom.Items.Add(i, Str);
+
+      // グラフデータの追加
+      Series1.Add(hour_count);
+      Series2.Add(total_count);
+   end;
+
+   with Chart1 do begin
+      // 左側目盛りの調整
+      Axes.Left.Maximum := ((hour_peak div 10) + 1) * 10;
+      if Axes.Left.Maximum <= 10 then begin
+         Axes.Left.Increment := 2;
+      end
+      else begin
+         Axes.Left.Increment := 20;
+      end;
+
+      // 右側目盛りの調整
+      Axes.Right.Maximum := ((total_count div 50) + 1) * 50;
+      if Axes.Right.Maximum <= 50 then begin
+         Axes.Right.Increment := 10;
+      end
+      else begin
+         Axes.Right.Increment := 50;
+      end;
+   end;
 end;
 
 procedure TRateDialog.ShowLastComboChange(Sender: TObject);
-var i : integer;
 begin
-  try
-    i := StrToInt(ShowLastCombo.Text);
-  except
-    on EConvertError do
-      i := 12;
-  end;
-  ShowLast := i;
-  UpdateGraph;
-end;
-
-procedure TRateDialog.FormClose(Sender: TObject; var Action: TCloseAction);
-begin
-  Timer.Enabled := False;
-end;
-
-procedure TRateDialog.CreateParams(var Params: TCreateParams);
-begin
-  inherited CreateParams(Params);
-  Params.ExStyle := Params.ExStyle or WS_EX_APPWINDOW;
+   FShowLast := StrToIntDef(ShowLastCombo.Items[ShowLastCombo.ItemIndex], 12);
+   UpdateGraph;
 end;
 
 end.
