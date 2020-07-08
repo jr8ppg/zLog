5bf03e006f149cac305fb9e6502a847d522f5a1b
jr8ppg <jr8ppg@gmail.com>
2020-07-08 21:39:58 +0900

    Fixed: SHIFT+B(b)等の入力が本体側に取られている件の修正

--- zlog/UCWKeyBoard.dfm
+++ zlog/UCWKeyBoard.dfm
@@ -11,7 +11,7 @@ object CWKeyBoard: TCWKeyBoard
   Font.Name = #65325#65331' '#12468#12471#12483#12463
   Font.Style = []
   OldCreateOrder = True
-  OnKeyDown = FormKeyDown
+  OnCreate = FormCreate
   OnShow = FormShow
   PixelsPerInch = 96
   TextHeight = 12
@@ -23,7 +23,6 @@ object CWKeyBoard: TCWKeyBoard
     Align = alClient
     ImeName = 'MS-IME97 '#26085#26412#35486#20837#21147#65404#65405#65411#65425
     TabOrder = 0
-    OnKeyDown = ConsoleKeyDown
     OnKeyPress = ConsoleKeyPress
   end
   object Panel1: TPanel
@@ -34,23 +33,172 @@ object CWKeyBoard: TCWKeyBoard
     Align = alBottom
     BevelOuter = bvNone
     TabOrder = 1
-    object Button1: TButton
+    object buttonOK: TButton
       Left = 6
       Top = 8
       Width = 65
       Height = 21
       Caption = 'OK'
       TabOrder = 0
-      OnClick = OKClick
+      OnClick = buttonOKClick
     end
-    object Button2: TButton
+    object buttonClear: TButton
       Left = 80
       Top = 8
       Width = 65
       Height = 21
       Caption = 'Clear'
       TabOrder = 1
-      OnClick = Button2Click
+      OnClick = buttonClearClick
+    end
+  end
+  object ActionList1: TActionList
+    Left = 240
+    Top = 40
+    object actionPlayMessageA01: TAction
+      Tag = 1
+      Caption = 'actionPlayMessageA01'
+      ShortCut = 112
+      OnExecute = actionPlayMessageAExecute
+    end
+    object actionPlayMessageA02: TAction
+      Tag = 2
+      Caption = 'actionPlayMessageA02'
+      ShortCut = 113
+      OnExecute = actionPlayMessageAExecute
+    end
+    object actionPlayMessageA03: TAction
+      Tag = 3
+      Caption = 'actionPlayMessageA03'
+      ShortCut = 114
+      OnExecute = actionPlayMessageAExecute
+    end
+    object actionPlayMessageA04: TAction
+      Tag = 4
+      Caption = 'actionPlayMessageA04'
+      ShortCut = 115
+      OnExecute = actionPlayMessageAExecute
+    end
+    object actionPlayMessageA05: TAction
+      Tag = 5
+      Caption = 'actionPlayMessageA05'
+      ShortCut = 116
+      OnExecute = actionPlayMessageAExecute
+    end
+    object actionPlayMessageA06: TAction
+      Tag = 6
+      Caption = 'actionPlayMessageA06'
+      ShortCut = 117
+      OnExecute = actionPlayMessageAExecute
+    end
+    object actionPlayMessageA07: TAction
+      Tag = 7
+      Caption = 'actionPlayMessageA07'
+      ShortCut = 118
+      OnExecute = actionPlayMessageAExecute
+    end
+    object actionPlayMessageA08: TAction
+      Tag = 8
+      Caption = 'actionPlayMessageA08'
+      ShortCut = 119
+      OnExecute = actionPlayMessageAExecute
+    end
+    object actionPlayMessageA11: TAction
+      Tag = 11
+      Caption = 'actionPlayMessageA11'
+      ShortCut = 122
+      OnExecute = actionPlayMessageAExecute
+    end
+    object actionPlayMessageA12: TAction
+      Tag = 12
+      Caption = 'actionPlayMessageA12'
+      ShortCut = 123
+      OnExecute = actionPlayMessageAExecute
+    end
+    object actionPlayMessageB01: TAction
+      Tag = 1
+      Caption = 'actionPlayMessageB01'
+      ShortCut = 8304
+      OnExecute = actionPlayMessageBExecute
+    end
+    object actionPlayMessageB02: TAction
+      Tag = 2
+      Caption = 'actionPlayMessageB02'
+      ShortCut = 8305
+      OnExecute = actionPlayMessageBExecute
+    end
+    object actionPlayMessageB03: TAction
+      Tag = 3
+      Caption = 'actionPlayMessageB03'
+      ShortCut = 8306
+      OnExecute = actionPlayMessageBExecute
+    end
+    object actionPlayMessageB04: TAction
+      Tag = 4
+      Caption = 'actionPlayMessageB04'
+      ShortCut = 8307
+      OnExecute = actionPlayMessageBExecute
+    end
+    object actionPlayMessageB05: TAction
+      Tag = 5
+      Caption = 'actionPlayMessageB05'
+      ShortCut = 8308
+      OnExecute = actionPlayMessageBExecute
+    end
+    object actionPlayMessageB06: TAction
+      Tag = 6
+      Caption = 'actionPlayMessageB06'
+      ShortCut = 8309
+      OnExecute = actionPlayMessageBExecute
+    end
+    object actionPlayMessageB07: TAction
+      Tag = 7
+      Caption = 'actionPlayMessageB07'
+      ShortCut = 8310
+      OnExecute = actionPlayMessageBExecute
+    end
+    object actionPlayMessageB08: TAction
+      Tag = 8
+      Caption = 'actionPlayMessageB08'
+      ShortCut = 8311
+      OnExecute = actionPlayMessageBExecute
+    end
+    object actionPlayMessageB11: TAction
+      Tag = 11
+      Caption = 'actionPlayMessageB11'
+      ShortCut = 8314
+      OnExecute = actionPlayMessageBExecute
+    end
+    object actionPlayMessageB12: TAction
+      Tag = 12
+      Caption = 'actionPlayMessageB12'
+      ShortCut = 8315
+      OnExecute = actionPlayMessageBExecute
+    end
+    object actionPlayMessageAR: TAction
+      Caption = 'actionPlayMessageAR'
+      ShortCut = 8257
+      OnExecute = actionPlayMessageARExecute
+    end
+    object actionPlayMessageBK: TAction
+      Caption = 'actionPlayMessageBK'
+      ShortCut = 8258
+      OnExecute = actionPlayMessageBKExecute
+    end
+    object actionPlayMessageKN: TAction
+      Caption = 'actionPlayMessageKN'
+      ShortCut = 8267
+      OnExecute = actionPlayMessageKNExecute
+    end
+    object actionPlayMessageSK: TAction
+      Caption = 'actionPlayMessageSK'
+      ShortCut = 8275
+      OnExecute = actionPlayMessageSKExecute
+    end
+    object actionESC: TAction
+      Caption = 'actionESC'
+      ShortCut = 27
+      OnExecute = actionESCExecute
     end
   end
 end
--- zlog/UCWKeyBoard.pas
+++ zlog/UCWKeyBoard.pas
@@ -4,33 +4,65 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  StdCtrls, ExtCtrls, ClipBrd,
+  StdCtrls, ExtCtrls, ClipBrd, System.Actions, Vcl.ActnList,
   UzLogConst, UzLogGlobal, UzLogQSO, UzLogCW, UzLogKeyer;
 
 type
   TCWKeyBoard = class(TForm)
     Console: TMemo;
     Panel1: TPanel;
-    Button1: TButton;
-    Button2: TButton;
+    buttonOK: TButton;
+    buttonClear: TButton;
+    ActionList1: TActionList;
+    actionPlayMessageA01: TAction;
+    actionPlayMessageA02: TAction;
+    actionPlayMessageA03: TAction;
+    actionPlayMessageA04: TAction;
+    actionPlayMessageA05: TAction;
+    actionPlayMessageA06: TAction;
+    actionPlayMessageA07: TAction;
+    actionPlayMessageA08: TAction;
+    actionPlayMessageA11: TAction;
+    actionPlayMessageA12: TAction;
+    actionPlayMessageB01: TAction;
+    actionPlayMessageB02: TAction;
+    actionPlayMessageB03: TAction;
+    actionPlayMessageB04: TAction;
+    actionPlayMessageB05: TAction;
+    actionPlayMessageB06: TAction;
+    actionPlayMessageB07: TAction;
+    actionPlayMessageB08: TAction;
+    actionPlayMessageB11: TAction;
+    actionPlayMessageB12: TAction;
+    actionPlayMessageAR: TAction;
+    actionPlayMessageBK: TAction;
+    actionPlayMessageKN: TAction;
+    actionPlayMessageSK: TAction;
+    actionESC: TAction;
     procedure ConsoleKeyPress(Sender: TObject; var Key: Char);
-    procedure FormKeyDown(Sender: TObject; var Key: Word;
-      Shift: TShiftState);
-    procedure OKClick(Sender: TObject);
+    procedure buttonOKClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
-    procedure ConsoleKeyDown(Sender: TObject; var Key: Word;
-      Shift: TShiftState);
     procedure CreateParams(var Params: TCreateParams); override;
-    procedure Button2Click(Sender: TObject);
+    procedure buttonClearClick(Sender: TObject);
+    procedure actionPlayMessageAExecute(Sender: TObject);
+    procedure actionPlayMessageBExecute(Sender: TObject);
+    procedure actionPlayMessageARExecute(Sender: TObject);
+    procedure actionPlayMessageBKExecute(Sender: TObject);
+    procedure actionPlayMessageKNExecute(Sender: TObject);
+    procedure actionPlayMessageSKExecute(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure actionESCExecute(Sender: TObject);
   private
     { Private declarations }
+    procedure PlayMessage(cb: Integer; no: Integer);
   public
     { Public declarations }
   end;
 
 implementation
 
-uses Main, UOptions;
+uses
+  Main;
 
 {$R *.DFM}
 
@@ -38,6 +70,58 @@ procedure TCWKeyBoard.CreateParams(var Params: TCreateParams);
 begin
    inherited CreateParams(Params);
    Params.ExStyle := Params.ExStyle or WS_EX_APPWINDOW;
+end;
+
+procedure TCWKeyBoard.FormCreate(Sender: TObject);
+begin
+   actionPlayMessageA01.ShortCut := MainForm.actionPlayMessageA01.ShortCut;
+   actionPlayMessageA02.ShortCut := MainForm.actionPlayMessageA02.ShortCut;
+   actionPlayMessageA03.ShortCut := MainForm.actionPlayMessageA03.ShortCut;
+   actionPlayMessageA04.ShortCut := MainForm.actionPlayMessageA04.ShortCut;
+   actionPlayMessageA05.ShortCut := MainForm.actionPlayMessageA05.ShortCut;
+   actionPlayMessageA06.ShortCut := MainForm.actionPlayMessageA06.ShortCut;
+   actionPlayMessageA07.ShortCut := MainForm.actionPlayMessageA07.ShortCut;
+   actionPlayMessageA08.ShortCut := MainForm.actionPlayMessageA08.ShortCut;
+   actionPlayMessageA11.ShortCut := MainForm.actionPlayMessageA11.ShortCut;
+   actionPlayMessageA12.ShortCut := MainForm.actionPlayMessageA12.ShortCut;
+
+   actionPlayMessageB01.ShortCut := MainForm.actionPlayMessageB01.ShortCut;
+   actionPlayMessageB02.ShortCut := MainForm.actionPlayMessageB02.ShortCut;
+   actionPlayMessageB03.ShortCut := MainForm.actionPlayMessageB03.ShortCut;
+   actionPlayMessageB04.ShortCut := MainForm.actionPlayMessageB04.ShortCut;
+   actionPlayMessageB05.ShortCut := MainForm.actionPlayMessageB05.ShortCut;
+   actionPlayMessageB06.ShortCut := MainForm.actionPlayMessageB06.ShortCut;
+   actionPlayMessageB07.ShortCut := MainForm.actionPlayMessageB07.ShortCut;
+   actionPlayMessageB08.ShortCut := MainForm.actionPlayMessageB08.ShortCut;
+   actionPlayMessageB11.ShortCut := MainForm.actionPlayMessageB11.ShortCut;
+   actionPlayMessageB12.ShortCut := MainForm.actionPlayMessageB12.ShortCut;
+
+   actionPlayMessageA01.SecondaryShortCuts.Assign(MainForm.actionPlayMessageA01.SecondaryShortCuts);
+   actionPlayMessageA02.SecondaryShortCuts.Assign(MainForm.actionPlayMessageA02.SecondaryShortCuts);
+   actionPlayMessageA03.SecondaryShortCuts.Assign(MainForm.actionPlayMessageA03.SecondaryShortCuts);
+   actionPlayMessageA04.SecondaryShortCuts.Assign(MainForm.actionPlayMessageA04.SecondaryShortCuts);
+   actionPlayMessageA05.SecondaryShortCuts.Assign(MainForm.actionPlayMessageA05.SecondaryShortCuts);
+   actionPlayMessageA06.SecondaryShortCuts.Assign(MainForm.actionPlayMessageA06.SecondaryShortCuts);
+   actionPlayMessageA07.SecondaryShortCuts.Assign(MainForm.actionPlayMessageA07.SecondaryShortCuts);
+   actionPlayMessageA08.SecondaryShortCuts.Assign(MainForm.actionPlayMessageA08.SecondaryShortCuts);
+   actionPlayMessageA11.SecondaryShortCuts.Assign(MainForm.actionPlayMessageA11.SecondaryShortCuts);
+   actionPlayMessageA12.SecondaryShortCuts.Assign(MainForm.actionPlayMessageA12.SecondaryShortCuts);
+
+   actionPlayMessageB01.SecondaryShortCuts.Assign(MainForm.actionPlayMessageB01.SecondaryShortCuts);
+   actionPlayMessageB02.SecondaryShortCuts.Assign(MainForm.actionPlayMessageB02.SecondaryShortCuts);
+   actionPlayMessageB03.SecondaryShortCuts.Assign(MainForm.actionPlayMessageB03.SecondaryShortCuts);
+   actionPlayMessageB04.SecondaryShortCuts.Assign(MainForm.actionPlayMessageB04.SecondaryShortCuts);
+   actionPlayMessageB05.SecondaryShortCuts.Assign(MainForm.actionPlayMessageB05.SecondaryShortCuts);
+   actionPlayMessageB06.SecondaryShortCuts.Assign(MainForm.actionPlayMessageB06.SecondaryShortCuts);
+   actionPlayMessageB07.SecondaryShortCuts.Assign(MainForm.actionPlayMessageB07.SecondaryShortCuts);
+   actionPlayMessageB08.SecondaryShortCuts.Assign(MainForm.actionPlayMessageB08.SecondaryShortCuts);
+   actionPlayMessageB11.SecondaryShortCuts.Assign(MainForm.actionPlayMessageB11.SecondaryShortCuts);
+   actionPlayMessageB12.SecondaryShortCuts.Assign(MainForm.actionPlayMessageB12.SecondaryShortCuts);
+end;
+
+procedure TCWKeyBoard.FormShow(Sender: TObject);
+begin
+   Console.SetFocus;
 end;
 
 procedure TCWKeyBoard.ConsoleKeyPress(Sender: TObject; var Key: Char);
@@ -61,65 +145,109 @@ begin
    end;
 end;
 
-procedure TCWKeyBoard.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
-begin
-   case Key of
-      VK_ESCAPE: begin
-         dmZLogKeyer.ControlPTT(False);
-         MainForm.LastFocus.SetFocus;
-      end;
-   end;
-end;
-
-procedure TCWKeyBoard.OKClick(Sender: TObject);
+procedure TCWKeyBoard.buttonOKClick(Sender: TObject);
 begin
    Close;
    MainForm.LastFocus.SetFocus;
 end;
 
-procedure TCWKeyBoard.FormShow(Sender: TObject);
+procedure TCWKeyBoard.buttonClearClick(Sender: TObject);
 begin
-   Console.SetFocus;
+   Console.Clear;
 end;
 
-procedure TCWKeyBoard.ConsoleKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+procedure TCWKeyBoard.actionPlayMessageAExecute(Sender: TObject);
 var
-   i: integer;
-   S: string;
+   no: Integer;
+   cb: Integer;
 begin
-   case Key of
-      VK_ESCAPE: begin
-         if dmZLogKeyer.IsPlaying then begin
-            dmZLogKeyer.ClrBuffer;
-            dmZLogKeyer.ControlPTT(False);
-         end
-         else begin
-            dmZLogKeyer.ControlPTT(False);
-            MainForm.SetFocus;
-            // Caption := 'CODE 2';
-         end;
-      end;
+   no := TAction(Sender).Tag;
+   cb := dmZlogGlobal.Settings.CW.CurrentBank;
 
-      VK_F1 .. VK_F8: begin
-         i := Key - VK_F1 + 1;
-         S := dmZlogGlobal.CWMessage(dmZlogGlobal.Settings.CW.CurrentBank, i);
-         S := SetStr(S, Main.CurrentQSO);
-         zLogSendStr(S);
+   {$IFDEF DEBUG}
+   OutputDebugString(PChar('PlayMessageA(' + IntToStr(cb) + ',' + IntToStr(no) + ')'));
+   {$ENDIF}
 
-         while Pos(':***********', S) > 0 do begin
-            i := Pos(':***********', S);
-            Delete(S, i, 12);
-            Insert(CurrentQSO.Callsign, S, i);
-         end;
-         ClipBoard.AsText := S;
-         Console.PasteFromClipBoard;
-      end;
+   PlayMessage(cb, no);
+end;
+
+procedure TCWKeyBoard.actionPlayMessageBExecute(Sender: TObject);
+var
+   no: Integer;
+   cb: Integer;
+begin
+   no := TAction(Sender).Tag;
+   cb := dmZlogGlobal.Settings.CW.CurrentBank;
+
+   if cb = 1 then
+      cb := 2
+   else
+      cb := 1;
+
+   {$IFDEF DEBUG}
+   OutputDebugString(PChar('PlayMessageB(' + IntToStr(cb) + ',' + IntToStr(no) + ')'));
+   {$ENDIF}
+
+   PlayMessage(cb, no);
+end;
+
+procedure TCWKeyBoard.actionPlayMessageARExecute(Sender: TObject);
+begin
+   dmZLogKeyer.SetCWSendBufCharPTT('a');
+   Console.Text := Console.Text + '[AR]';
+   Console.SelStart := Length(Console.Text);
+end;
+
+procedure TCWKeyBoard.actionPlayMessageBKExecute(Sender: TObject);
+begin
+   dmZLogKeyer.SetCWSendBufCharPTT('b');
+   Console.Text := Console.Text + '[BK]';
+   Console.SelStart := Length(Console.Text);
+end;
+
+procedure TCWKeyBoard.actionPlayMessageKNExecute(Sender: TObject);
+begin
+   dmZLogKeyer.SetCWSendBufCharPTT('k');
+   Console.Text := Console.Text + '[KN]';
+   Console.SelStart := Length(Console.Text);
+end;
+
+procedure TCWKeyBoard.actionPlayMessageSKExecute(Sender: TObject);
+begin
+   dmZLogKeyer.SetCWSendBufCharPTT('s');
+   Console.Text := Console.Text + '[SK]';
+   Console.SelStart := Length(Console.Text);
+end;
+
+procedure TCWKeyBoard.actionESCExecute(Sender: TObject);
+begin
+   if dmZLogKeyer.IsPlaying then begin
+      dmZLogKeyer.ClrBuffer;
+      dmZLogKeyer.ControlPTT(False);
+   end
+   else begin
+      dmZLogKeyer.ControlPTT(False);
+      MainForm.LastFocus.SetFocus;
    end;
 end;
 
-procedure TCWKeyBoard.Button2Click(Sender: TObject);
+procedure TCWKeyBoard.PlayMessage(cb: Integer; no: Integer);
+var
+   S: string;
+   i: Integer;
 begin
-   Console.Clear;
+   S := dmZlogGlobal.CWMessage(cb, no);
+   S := SetStr(S, CurrentQSO);
+   zLogSendStr(S);
+
+   while Pos(':***********', S) > 0 do begin
+      i := Pos(':***********', S);
+      Delete(S, i, 12);
+      Insert(CurrentQSO.Callsign, S, i);
+   end;
+
+   ClipBoard.AsText := S;
+   Console.PasteFromClipBoard;
 end;
 
 end.
