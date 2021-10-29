8022079e4f2b0203a578c5f5e6737b8672f7514c
jr8ppg <jr8ppg@gmail.com>
2021-10-29 23:59:30 +0900

    Changed: Quick Referenceの文字サイズを変更できるように修正 Issues #222

--- zlog/UQuickRef.pas
+++ zlog/UQuickRef.pas
@@ -4,14 +4,21 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, UzLogGlobal;
+  Dialogs, StdCtrls, UzLogGlobal, Vcl.ComCtrls, Vcl.ToolWin, System.ImageList,
+  Vcl.ImgList;
 
 type
   TQuickRef = class(TForm)
     Memo: TMemo;
+    ToolBar1: TToolBar;
+    toolbuttonPlus: TToolButton;
+    toolbuttonMinus: TToolButton;
+    ImageList1: TImageList;
     procedure FormCreate(Sender: TObject);
     procedure MemoKeyPress(Sender: TObject; var Key: Char);
     procedure FormDestroy(Sender: TObject);
+    procedure toolbuttonPlusClick(Sender: TObject);
+    procedure toolbuttonMinusClick(Sender: TObject);
   private
     { Private êÈåæ }
   public
@@ -33,6 +40,9 @@ begin
    end
    else
       Memo.Lines.Clear;
+
+   Memo.Font.Size := dmZLogGlobal.Settings.FQuickRefFontSize;
+   Memo.Font.Name := dmZLogGlobal.Settings.FQuickRefFontFace;
 end;
 
 procedure TQuickRef.FormDestroy(Sender: TObject);
@@ -44,6 +54,34 @@ procedure TQuickRef.MemoKeyPress(Sender: TObject; var Key: Char);
 begin
    if Key = Chr($1B) then
       MainForm.LastFocus.SetFocus;
+end;
+
+procedure TQuickRef.toolbuttonPlusClick(Sender: TObject);
+var
+   n: Integer;
+begin
+   n := Memo.Font.Size;
+   if n < 20 then begin
+      Inc(n);
+   end;
+   Memo.Font.Size := n;
+
+   dmZLogGlobal.Settings.FQuickRefFontSize := n;
+   //dmZLogGlobal.Settings.FQuickRefFontFace := Memo.Font.Name;
+end;
+
+procedure TQuickRef.toolbuttonMinusClick(Sender: TObject);
+var
+   n: Integer;
+begin
+   n := Memo.Font.Size;
+   if n > 8 then begin
+      Dec(n);
+   end;
+   Memo.Font.Size := n;
+
+   dmZLogGlobal.Settings.FQuickRefFontSize := n;
+   //dmZLogGlobal.Settings.FQuickRefFontFace := Memo.Font.Name;
 end;
 
 end.
