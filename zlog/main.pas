cb3fc559967b332d92851213226b1804ed4afd4d
jr8ppg <jr8ppg@gmail.com>
2023-01-21 19:23:26 +0900

    Fixed: XIT未サポート機種でも AntiZeroinしたメッセージが出てしまう

--- zlog/main.pas
+++ zlog/main.pas
@@ -8497,6 +8497,12 @@ begin
    if CurrentQSO.CQ = True then begin
       Exit;
    end;
+   if RigControl.Rig = nil then begin
+      Exit;
+   end;
+   if RigControl.Rig.XitCtrlSupported = False then begin
+      Exit;
+   end;
 
    Randomize();
 
