64d2e908c625e78f95c159e2e4838543d78cc863
jr8ppg <jr8ppg@gmail.com>
2024-11-26 23:46:12 +0900

    Fixed: QSOを削除するとアプリケーションエラーが出る件の修正（別スレッドの場合） #719

--- zlog/main.pas
+++ zlog/main.pas
@@ -13889,8 +13889,13 @@ begin
    Inherited Create(True);
    FreeOnTerminate := True;
    FParent := formParent;
-   FQso := TQSO.Create();
-   FQso.Assign(aQSO);
+   if aQSO <> nil then begin
+      FQso := TQSO.Create();
+      FQso.Assign(aQSO);
+   end
+   else begin
+      FQso := nil;
+   end;
 end;
 
 procedure TBandScopeNotifyThread.Execute();
