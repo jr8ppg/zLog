fe340e3989cb89cb25ed161f3709b559fb9a994a
jr8ppg <jr8ppg@gmail.com>
2025-05-23 23:02:25 +0900

    Changed: QSO編集ダイアログに送信RSTが欲しい（４） #768 SHIFT+R/S手直し

--- zlog/main.pas
+++ zlog/main.pas
@@ -11184,15 +11184,27 @@ end;
 // #93 了解度(R)変更 Shift+R
 procedure TMainForm.actionChangeRExecute(Sender: TObject);
 begin
-   CurrentQSO.RSTRcvd := IncreaseR(CurrentQSO.RSTRcvd);
-   RcvdRSTEdit.Text := CurrentQSO.RSTStr;
+   if RcvdRSTEdit.Focused then begin
+      CurrentQSO.RSTRcvd := IncreaseR(CurrentQSO.RSTRcvd);
+      RcvdRSTEdit.Text := CurrentQSO.RSTStr;
+   end;
+   if SentRSTEdit.Focused then begin
+      CurrentQSO.RSTSent := IncreaseR(CurrentQSO.RSTSent);
+      SentRSTEdit.Text := CurrentQSO.RSTSentStr;
+   end;
 end;
 
 // #94 信号強度(S)変更 Shift+S
 procedure TMainForm.actionChangeSExecute(Sender: TObject);
 begin
-   CurrentQSO.RSTRcvd := IncreaseS(CurrentQSO.RSTRcvd);
-   RcvdRSTEdit.Text := CurrentQSO.RSTStr;
+   if RcvdRSTEdit.Focused then begin
+      CurrentQSO.RSTRcvd := IncreaseS(CurrentQSO.RSTRcvd);
+      RcvdRSTEdit.Text := CurrentQSO.RSTStr;
+   end;
+   if SentRSTEdit.Focused then begin
+      CurrentQSO.RSTSent := IncreaseS(CurrentQSO.RSTSent);
+      SentRSTEdit.Text := CurrentQSO.RSTSentStr;
+   end;
 end;
 
 // #95 時刻フィールドを現在時刻にセットする Shift+T
