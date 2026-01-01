f3b9ece5c4ffe489611e6c77125c1843244420aa
jr8ppg <jr8ppg@gmail.com>
2026-01-01 22:02:46 +0900

    Changed: ダークモードの実装（５） #783

--- zlog/main.pas
+++ zlog/main.pas
@@ -1514,6 +1514,7 @@ resourcestring
   TMainForm_To_Change_the_mode = 'To change the operating mode, first turn off the F2A mode.';  // 'モードを変更するには、先にF2Aモードをoffにして下さい'
   TMainForm_To_Change_the_band = 'To change the operating band, first turn off the F2A mode.';  // 'バンドを変更するには、先にF2Aモードをoffにして下さい'
   TMainform_rbn_verified = 'Verification results' + #13#10 + '%1 of %2 stations were verified with RBN';
+  TMainform_darmode_changed = 'Dark Mode changes will take effect on the next startup.';  // ダークモードの変更は次の起動時に反映されます。
 
 var
   MainForm: TMainForm;
@@ -6649,12 +6650,7 @@ begin
       dmZlogGlobal.SaveCurrentSettings();
 
       if fPrevDarkMode <> dmZLogGlobal.Settings.FUseDarkMode then begin
-         if dmZLogGlobal.Settings.FUseDarkMode = True then begin
-            SetDarkMode();
-         end
-         else begin
-            SetLightMode();
-         end;
+         MessageBox(Handle, PChar(TMainform_darmode_changed), PChar(Application.Title), MB_OK or MB_ICONEXCLAMATION);
       end;
 
       InitBandMenu();
