aeed6495cb7448b503f654d37a498b1a524fbb86
jr8ppg <jr8ppg@gmail.com>
2026-02-11 20:47:10 +0900

    Changed: HAMLOG連携（２） #933 二重起動防止

--- HamlogLookup/HamlogLookup.dpr
+++ HamlogLookup/HamlogLookup.dpr
@@ -3,7 +3,8 @@ program HamlogLookup;
 
 
 uses
-  Forms,
+  Vcl.Forms,
+  WinApi.Windows,
   Main in 'Main.pas' {formHamlogLookup},
   Progress in 'Progress.pas' {formProgress},
   SelectZlog in 'SelectZlog.pas' {formSelectZLog},
@@ -14,10 +15,24 @@ uses
 
 {$R *.res}
 
+const
+  MutexName = 'zLog_HamlogLookup';
+
+var
+  hMutex: THANDLE;
+
 begin
+  hMutex := OpenMutex(MUTEX_ALL_ACCESS, False, MutexName);
+  if hMutex <> 0 then begin
+    CloseHandle(hMutex);
+    Exit;
+  end;
+  hMutex := CreateMutex(nil, False, MutexName);
+
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.Title := 'HAMLOGLookup';
   Application.CreateForm(TformHamlogLookup, formHamlogLookup);
   Application.Run;
+  ReleaseMutex(hMutex);
 end.
